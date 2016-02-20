1512c2953bcb3d6ee7f5719502fae15cd4cbeb2c
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2016-02-20 13:11:37 +0200

    Added initial functionality for Telerik Backend Services

--- app/app.js
+++ app/app.js
@@ -1,4 +1,8 @@
 var application = require("application");
+
+global.profileCreated = 'profileCreated';
+global.userId = 'userId';
+
 application.mainModule = "login-page";
 application.cssFile = "./app.css";
 application.start();
