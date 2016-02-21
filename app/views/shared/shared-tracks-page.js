19c9a11c1ddbb1aad2575251d34ece53faec5a11
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2016-02-21 20:44:35 +0200

    Implemented functionality for getting shared tracks

--- app/views/shared/shared-tracks-page.js
+++ app/views/shared/shared-tracks-page.js
@@ -1,11 +1,17 @@
 var frameModule = require("ui/frame");
 var observable = require("data/observable");
+var Everlive = require('./../../libs/everlive/everlive.all.min');
+var el = new Everlive('nh2gqgfwwjk2l3nj');
+var appSettings = require('application-settings');
 
 var topmost;
+
+var myTracks = [];
 
 function pageLoaded(args) {
 	var page = args.object;
 	topmost = frameModule.topmost();
+	myTracks = getSharedTracks();
 }
 
 function tapTrack() {
@@ -19,6 +25,29 @@ function tapProfile() {
 function tapShared() {
 	topmost.navigate("views/shared/shared-tracks-page");
 }
+
+function getSharedTracks(){
+	var data = el.data('track');
+	var query = new Everlive.Query();
+	
+	query.where()
+		.eq('isPublic', true)
+		.done()
+		.select('userFullName', 'distance', 'trackPictureUrl')
+//		.orderDesc('Created-at')
+		.take(1);
+
+
+		data.get(query)
+				.then(function(data) {
+					console.log("-------------------success---------------");
+					console.log(JSON.stringify(data.result));
+					return data.result;
+				}, function(error) {
+					console.log(JSON.stringify(error));
+				});
+}
+
 
 exports.tapTrack = tapTrack;
 exports.tapProfile = tapProfile;
